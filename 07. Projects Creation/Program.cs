68db24a079b7b17b95ba0349840c239144f20cd6
energycourses <151967092+energy-courses@users.noreply.github.com>
2023-12-03 16:09:20 +0200

    Add files via upload

---
+++ 07. Projects Creation/Program.cs
@@ -0,0 +1,4 @@
+﻿string name = Console.ReadLine();
+double projects = double.Parse(Console.ReadLine()); 
+double time = 3 *  projects;
+Console.WriteLine($"The architect {name} will need {time} hours to complete {projects} project/s.");
---
+++ 08. Pet Shop/Program.cs
@@ -0,0 +1,6 @@
+﻿double dog = double.Parse(Console.ReadLine());
+double cat = double.Parse(Console.ReadLine());
+double dogFood = dog * 2.5;
+double catFood = cat * 4;
+double total = dogFood + catFood;
+Console.WriteLine($"{total} lv.");
